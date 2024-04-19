25e5daf69a35e8ad4ff16972177a2c5d92aaf118
user <user@host.docker.internal>
2024-04-19 21:52:02 +0900

    스프링 프로젝트 기초 설정

---
+++ wiz4/src/test/java/com/choongangclass601team4/mapper/BoardMapperTests.java
@@ -0,0 +1,26 @@
+package com.choongangclass601team4.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {com.choongangclass601team4.config.RootConfig.class})
+@Log4j
+public class BoardMapperTests {
+	
+	@Setter(onMethod_ = @Autowired)
+	private BoardMapper mapper;
+	
+	@Test
+	public void testGetList() {
+	
+		mapper.getList().forEach(board -> log.info(board));
+	}
+
+}
